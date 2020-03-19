1846e5b955fe19a611f1efcfd513d529f6c4427b
Biabiany <damienbiabiany@yahoo.fr>
2020-03-19 15:23:37 +0100

    radioButtons event listener

--- app/src/main/java/com/damien/flashcard/MainActivity.java
+++ app/src/main/java/com/damien/flashcard/MainActivity.java
@@ -3,6 +3,10 @@ package com.damien.flashcard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.graphics.Typeface;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onRadioButtonClicked(View  view) {
+
+        //require to import the RadioButton class
+        RadioButton rb1 = findViewById(R.id.first);
+        RadioButton rb2 = findViewById(R.id.second);
+        RadioButton rb3 = findViewById(R.id.third);
+
+
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.first:
+                if (checked)
+                    Log.i("first","first");
+                    // Set font
+                    rb1.setTypeface(null, Typeface.BOLD_ITALIC);
+                    rb2.setTypeface(null, Typeface.NORMAL);
+                    rb3.setTypeface(null, Typeface.NORMAL);
+                break;
+            case R.id.second:
+                if (checked)
+                    Log.i("second","second");
+                    // Set font
+                    rb1.setTypeface(null, Typeface.NORMAL);
+                    rb2.setTypeface(null, Typeface.BOLD_ITALIC);
+                    rb3.setTypeface(null, Typeface.NORMAL);
+                break;
+            case R.id.third:
+                if (checked)
+                    Log.i("third","third");
+                    // Set font
+                    rb1.setTypeface(null, Typeface.NORMAL);
+                    rb2.setTypeface(null, Typeface.NORMAL);
+                    rb3.setTypeface(null, Typeface.BOLD_ITALIC);
+                break;
+        }
     }
 }
