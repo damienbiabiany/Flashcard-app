efe8a8431405559ba5911d360fe633af86c6a5f2
Biabiany <damienbiabiany@yahoo.fr>
2020-03-19 17:25:40 +0100

    update question and response random

--- app/src/main/java/com/damien/flashcard/MainActivity.java
+++ app/src/main/java/com/damien/flashcard/MainActivity.java
@@ -7,6 +7,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.graphics.Typeface;
+import android.widget.RadioGroup;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +19,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // responses list
+        /*
+        ArrayList<String>  mylist = new ArrayList<String>();
+        mylist.add("code");
+        mylist.add("quiz");
+        mylist.add("geeksforgeeks");
+
+        Collections.shuffle(mylist);
+
+        // SET QUESTION
+        // random question
+        TextView question = findViewById(R.id.QuestionTextView);
+        question.setText("Test");
+
+
+        */
+
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
+        int count = radioGroup.getChildCount();
+        ArrayList<RadioButton> listOfRadioButtons = new ArrayList<RadioButton>();
+        for (int i=0;i<count;i++) {
+            View o = radioGroup.getChildAt(i);
+            if (o instanceof RadioButton) {
+                Log.i("child", "child"+ o);
+
+                listOfRadioButtons.add((RadioButton)o);
+
+                ((RadioButton)radioGroup.getChildAt(i)).setText("TEST" + i);
+            }
+        }
+
     }
+
 
     public void onRadioButtonClicked(View  view) {
 
@@ -32,11 +70,13 @@ public class MainActivity extends AppCompatActivity {
         switch(view.getId()) {
             case R.id.first:
                 if (checked)
-                    Log.i("first","first");
                     // Set font
                     rb1.setTypeface(null, Typeface.BOLD_ITALIC);
                     rb2.setTypeface(null, Typeface.NORMAL);
                     rb3.setTypeface(null, Typeface.NORMAL);
+
+                    String answer1 = rb1.getText().toString();
+                    Log.i("answer1", answer1);
                 break;
             case R.id.second:
                 if (checked)
@@ -45,15 +85,22 @@ public class MainActivity extends AppCompatActivity {
                     rb1.setTypeface(null, Typeface.NORMAL);
                     rb2.setTypeface(null, Typeface.BOLD_ITALIC);
                     rb3.setTypeface(null, Typeface.NORMAL);
+
+                    String answer2 = rb2.getText().toString();
+                    Log.i("answer2", answer2);
                 break;
             case R.id.third:
                 if (checked)
                     Log.i("third","third");
-                    // Set font
-                    rb1.setTypeface(null, Typeface.NORMAL);
-                    rb2.setTypeface(null, Typeface.NORMAL);
-                    rb3.setTypeface(null, Typeface.BOLD_ITALIC);
+                // Set font
+                rb1.setTypeface(null, Typeface.NORMAL);
+                rb2.setTypeface(null, Typeface.NORMAL);
+                rb3.setTypeface(null, Typeface.BOLD_ITALIC);
+
+                String answer3 = rb3.getText().toString();
+                Log.i("answer3", answer3);
                 break;
+
         }
     }
 }
